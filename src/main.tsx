32e63199e80d2fdc1805074a9f2f88293fa9efc9
Alberto Vilar <albertovilar1@outlook.com>
2025-05-22 12:21:19 -0300

    feat: Implement root layout with Outlet

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,25 @@ import {
 } from "react-router-dom";
 
 // Importa os componentes de rota
-import Root from "./routes/root.tsx";
+import Root from "./routes/Root/root.tsx";
 import ErrorPage from "./Error-peges/error-page.tsx";
 import Contact from "./routes/Contact/contact.tsx"; 
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    
+    children: [ // <-- Esta é a propriedade 'children' da rota pai (Root)
+      {
+        path: "contacts/:contactId", // <-- Este caminho é relativo ao pai ("/")
+        element: <Contact />,
+      },
+      // Para outras rotas filhas do Root, se houver
+    ],
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />, // <-- Agora o componente Contact estará disponível
-  },
+  // Para rotas que não são filhas da raiz, elas viriam aqui (fora do 'children' do Root)
 ]);
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/routes/Contact/contact.tsx
+++ src/routes/Contact/contact.tsx
@@ -69,6 +69,7 @@ export default function Contact() {
           </Form>
         </div>
       </div>
+      
     </div>
   );
 }
